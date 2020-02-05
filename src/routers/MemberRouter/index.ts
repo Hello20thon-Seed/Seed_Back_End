b8f77c77add3145745298000a79d29f33a651ffb
SkyLightQP <combbm@naver.com>
2020-02-05 22:47:47 +0900

    fix: 초대 로직 수정

--- src/routers/MemberRouter/index.ts
+++ src/routers/MemberRouter/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import {logger} from '../../index';
 import Goals from '../../databases/models/goals';
 import Users from '../../databases/models/users';
+import Forks from '../../databases/models/forks';
 import checkParams from "../../middlewares/CheckParams";
 import checkBody from "../../middlewares/CheckBody";
 
@@ -28,7 +29,9 @@ router.put('/:id', checkParams, checkBody, async (req, res) => {
     try {
         const userData = await Users.findOne({email});
         const goal = await Goals.findOne({_id: id});
+        const forkGoal = await Forks.findOne({ originId: id });
         const prevMembers = goal!.members;
+        const prevGoals = userData!.goal;
 
         if (prevMembers.includes(userData!)) {
             res.status(200).send({success: false, code: 402});
@@ -36,8 +39,10 @@ router.put('/:id', checkParams, checkBody, async (req, res) => {
         }
 
         prevMembers.push(userData!);
+        prevGoals.push(forkGoal!);
 
         await Goals.updateOne({_id: id}, {members: prevMembers});
+        await Users.updateOne({email}, {goal: prevGoals});
 
         logger.info(`${id} 원본 목표에 ${email} 유저를 초대합니다.`);
         res.status(200).send({success: true, code: 0});
@@ -54,12 +59,18 @@ router.delete('/:id', checkParams, checkBody, async (req, res) => {
     try {
         const userData = await Users.findOne({email});
         const goal = await Goals.findOne({_id: id});
+        const forkGoal = await Forks.findOne({ originId: id });
         const prevMembers = goal!.members;
+        const prevGoals = userData!.goal;
 
         const deleteIndex = prevMembers.findIndex((user) => user === userData);
         if (deleteIndex > -1) prevMembers.splice(deleteIndex, 1);
 
+        const deleteIndex2 = prevGoals.findIndex((g) => g === forkGoal);
+        if (deleteIndex2 > -1) prevGoals.splice(deleteIndex2, 1);
+
         await Goals.updateOne({_id: id}, {members: prevMembers});
+        await Users.updateOne({email}, {goal: prevGoals});
 
         logger.info(`${id} 원본 목표에 ${email} 유저를 초대 해제합니다.`);
         res.status(200).send({success: true, code: 0});
